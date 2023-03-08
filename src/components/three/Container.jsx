60da54da3c221d6fae246852ea71aa89fd0b4101
unknown <partharora557@gmail.com>
2023-03-08 10:31:17 +0530

    checking

--- src/components/three/Container.jsx
+++ src/components/three/Container.jsx
@@ -1,30 +1,16 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PresentationControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import * as THREE from "three";
 import Effects from "./effect/effect";
 import Ground from "./ground/ground";
 import Ring from "./thetaRing/ring";
-const Container = () => {
-  const [devicePixelRatio, setDevicePixelRatio] = React.useState();
 
-  React.useEffect(() => {
-    const pixel = window.devicePixelRatio;
-    setDevicePixelRatio(pixel);
-  }, []);
+const Scene = () => {
   return (
-    <Canvas
-      camera={{ position: [10, 0, 0], fov: 65 }}
-      dpr={devicePixelRatio}
-      gl={{ antialias: false }}
-      onCreated={({ gl }) => {
-        gl.toneMapping = THREE.ACESFilmicToneMapping;
-        gl.outputEncoding = THREE.sRGBEncoding;
-      }}
-      style={{ width: "100%", height: "100vh" }}
-    >
-      <color attach="background" args={["#3b082c"]} />
-      <fog color="#3b082c" attach="fog" near={8} far={30} />
+    <>
+      <color attach="background" args={["#2b0032"]} />
+      <fog color="#2b0032" attach="fog" near={8} far={30} />
       <ambientLight intensity={0.5} />
       <spotLight position={[0, 10, 0]} intensity={0.3} />
       <Suspense fallback={null}>
@@ -37,8 +23,32 @@ const Container = () => {
         <Ring id={3} i={7} />
         <Ring id={4} i={8} />
       </Suspense>
-      {/* <Ground /> */}
-      <OrbitControls maxDistance={100} minDistance={5} />
+      <Ground />
+      <OrbitControls maxDistance={100} minDistance={5} minPolarAngle={0} maxPolarAngle={Math.PI/2} />
+    </>
+  );
+};
+
+const Container = () => {
+  const [devicePixelRatio, setDevicePixelRatio] = React.useState();
+
+  React.useEffect(() => {
+    const pixel = window.devicePixelRatio;
+    setDevicePixelRatio(pixel);
+  }, []);
+  return (
+    <Canvas
+      camera={{ position: [0, 0, 0], fov: 65 }}
+      dpr={devicePixelRatio}
+      gl={{ antialias: false }}
+      onCreated={({ gl }) => {
+        gl.toneMapping = THREE.ACESFilmicToneMapping;
+        gl.outputEncoding = THREE.sRGBEncoding;
+      }}
+      style={{ width: "100%", height: "100vh" }}
+    ><Suspense fallback={null} >
+        <Scene/>
+    </Suspense>
     </Canvas>
   );
 };
